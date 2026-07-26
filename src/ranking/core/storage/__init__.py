f76c154bf76c99c0a3b85cd1e317f91068cd2299
Fabien <fletort@gmail.com>
2026-07-26 18:51:38 +0200

    Refactor to support custom boto3 S3 config #76

--- src/ranking/core/storage/__init__.py
+++ src/ranking/core/storage/__init__.py
@@ -3,5 +3,14 @@ from __future__ import annotations
 from ranking.core.storage.local import LocalStorageProvider
 from ranking.core.storage.provider import DownloadedDocument, StorageProvider
 from ranking.core.storage.s3 import S3StorageProvider
+from ranking.core.storage.s3_aws import AwsS3StorageProvider
+from ranking.core.storage.s3_gcs import GcsS3StorageProvider
 
-__all__ = ["LocalStorageProvider", "S3StorageProvider", "StorageProvider", "DownloadedDocument"]
+__all__ = [
+    "LocalStorageProvider",
+    "S3StorageProvider",
+    "AwsS3StorageProvider",
+    "GcsS3StorageProvider",
+    "StorageProvider",
+    "DownloadedDocument",
+]
---
+++ src/ranking/core/storage/s3_aws.py
@@ -0,0 +1,7 @@
+from .s3 import S3StorageProvider
+
+
+class AwsS3StorageProvider(S3StorageProvider):
+    """StorageProvider backed by Amazon S3 using the S3 API."""
+
+    pass
